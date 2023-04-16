556f173a5dafab74fa89b5951d32e4089b1b4938
Damtob <damtob4rill@gmail.com>
2023-04-16 15:23:32 +0100

    converting to naira

--- api.py
+++ api.py
@@ -5,7 +5,8 @@ API_KEY = '5c36bb01-2e42-40f5-b0ed-ef57cc2c9f9d'
 URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 # Set the parameters for the API request
-params = {'symbol': 'ETH', 'convert': 'USD'}
+params = {'symbol': 'ETH', 'convert': 'NGN'}
+
 
 # Set the headers with your API key
 headers = {'X-CMC_PRO_API_KEY':  API_KEY}
@@ -15,7 +16,9 @@ response = requests.get(URL, params=params, headers=headers)
 data = response.json()
 
 # Extract the Ethereum price from the response
-price = data['data']['ETH']['quote']['USD']['price']
+price_eth = data['data']['ETH']['quote']['NGN']['price']
+
+
 
 # Print the Ethereum price
-print(f"The current Ethereum price is ${price:.5f}")
+print(f"The current Ethereum price is N{price_eth:.5f}")
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ the response content as a JSON object. The resulting JSON object is stored in th
 variable.
 '''
 api_request = requests.get(
-    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=3889ce63-c733-403d-8b7d-c41a2438b4ea")
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=NGN&CMC_PRO_API_KEY=3889ce63-c733-403d-8b7d-c41a2438b4ea")
 api = json.loads(api_request.content)
 
 #These lines simply print a separator to visually separate the output.
@@ -56,19 +56,19 @@ for i in range(0, 4):
     for coin in coins:
         if api["data"][i]["symbol"] == coin["symbol"]:
             total_paid = coin["amount_owned"] * coin["price_per_coin"]
-            current_value = coin["amount_owned"] * api["data"][i]["quote"]["USD"]["price"]
-            pl_percoin = api["data"][i]["quote"]["USD"]["price"] - coin["price_per_coin"]
+            current_value = coin["amount_owned"] * api["data"][i]["quote"]["NGN"]["price"]
+            pl_percoin = api["data"][i]["quote"]["NGN"]["price"] - coin["price_per_coin"]
             total_pl_coin = pl_percoin * coin["amount_owned"]
 
             total_pl = total_pl + total_pl_coin
 
             print(api["data"][i]["name"] + " - " + api["data"][i]["symbol"])
-            print("Price - ${0:.3f}".format(api["data"][i]["quote"]["USD"]["price"]))
+            print("Price - N{0:.3f}".format(api["data"][i]["quote"]["NGN"]["price"]))
             print("Number Of Coin:", coin["amount_owned"])
             print("Total Amount Paid:", "${0:.3f}".format(total_paid))
-            print("Current Value:", "${0:.3f}".format(current_value))
-            print("P/L Per Coin:", "${0:.3f}".format(pl_percoin))
-            print("Total P/L With Coin:", "${0:.3f}".format(total_pl_coin))
+            print("Current Value:", "N{0:.3f}".format(current_value))
+            print("P/L Per Coin:", "N{0:.3f}".format(pl_percoin))
+            print("Total P/L With Coin:", "N{0:.3f}".format(total_pl_coin))
             print("----------------")
 
-print("Total P/L For Portfolio:", "${0:.3f}".format(total_pl))
+print("Total P/L For Portfolio:", "N{0:.3f}".format(total_pl))
